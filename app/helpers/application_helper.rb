84a577f18ef60c04345d4a770e9e6eda662bb628
Paul Cook <paul.cook@alternateversion.com>
2013-01-22 10:03:11 -0500

    generated user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def handle_notices(ignore=nil)
+    flsh = ''
+    flash.each do |key,msg|
+      flsh << '<div class="alert notice alert-block alert-'+key.to_s+'"><a class="close" data-dismiss="alert" href="#">&times;</a>'+simple_format(msg)+'</div>' unless ignore and ignore.include?(key)
+    end
+    flsh << '<div class="alert notice" style="display:none;"><a class="close" data-dismiss="alert" href="#">&times;</a></div>'
+    flsh.html_safe
+  end
 end
