48eb2544c31386ebcd1a98d2766986ebb50b9411
Paul Cook <paul.cook@alternateversion.com>
2013-01-22 09:32:26 -0500

    started project and added devise to gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracker::Application.config.secret_token = '7cb1c3c75599705c984d2b7bdd6fcae0c97dba74481485b06c45032a86bf41a573efcc4e66e1020830be8624124d584db733360eba82be0faf16d27deefcaeb5'
